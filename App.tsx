215cb236bea8f3cb1a6bc3145b4549047ceb6fb9
prosperatech1 <prosperatech1@gmail.com>
2026-04-23 21:56:44 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,30 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Home from './app/index';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>🆘 SOSCLT</Text>
-      <Text style={styles.subtitle}>Seu app de proteção trabalhista</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Home />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 10,
-  },
-  subtitle: {
-    fontSize: 16,
-    color: '#666',
-  },
-});
